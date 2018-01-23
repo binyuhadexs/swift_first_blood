fdbaf895674f36ec677a8800ad2b3ec68c7593e1
binyu.xia <binyu.xia@hiwifi.tw>
2018-01-23 14:01:36 +0800

    first commit

---
+++ SwiftFistBlood/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftFistBlood
+//
+//  Created by binyu on 2017/11/16.
+//  Copyright © 2017年 binyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("gogogo");
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
